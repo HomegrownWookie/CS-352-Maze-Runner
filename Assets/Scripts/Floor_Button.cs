8e8286edbf9d0a445cd759ae4360bcba7902dc31
Unknown <pakasteiner@gmail.com>
2018-05-09 00:08:16 -0700

    Initial Push
    This is the initial fork of the project.
    - I will be altering the project to make sure there is nothing we don't need to use

---
+++ Assets/Scripts/Floor_Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor_Button : Maze {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit clicked;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool isContained = false;
+
+            if (Physics.Raycast(ray, out clicked, 100.0f))
+            {
+                GameObject tempFloor = clicked.transform.gameObject;
+
+                foreach (Cell cell in allNodes)
+                {
+                    if (cell.cellFloor == tempFloor) isContained = true;
+                }
+                if (clicked.transform && isContained)
+                {
+                    clicked.transform.gameObject.GetComponent<Renderer>().material.color = Color.red;
+                }
+            }
+        }
+    }
+}
